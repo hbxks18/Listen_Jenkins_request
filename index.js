ab4f68d3df93728436392830f765d3ce2c53c93c
huangbinxu <BinxuHuang@sf-express.com>
2019-04-02 19:54:45 +0800

    fix: bug

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const app = express();
 const request = require('request');
 const exec = require('child_process').exec;
 
-const cmdStr = 'cat "~/.config/wechat_web_devtools/Default/.ide"';
+const cmdStr = 'cat "/root/.config/wechat_web_devtools/Default/.ide"';
 
 app.get('/', (req, res) => {
     exec(cmdStr, (err, stdout, stderr) => {
+        if (err) {
+            console.log('cmd执行错误', err);
+        }
+        if (stdout) {
+            console.log('端口号读取失败', stdout);
+        }
         const prot = stdout;
         const options = {
             method: 'GET',
@@ -14,7 +20,7 @@ app.get('/', (req, res) => {
         };
         request(options)
             .on('error', (err) => {
-                console.log(err)
+                console.log('登录二维码获取失败', err)
             })
             .pipe(res);
     })
--- start.sh
+++ start.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
-if which yarn 2>/dev/null; then
-    echo 'yarn \c'
-    yarn -v
-    yarn --ignore-engines
-    yarn start
-else
-    echo 'npm \c'
-    npm -v
-    npm install
-    npm run start
-fi
+npm -v
+npm install
+npm run start
