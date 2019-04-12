39e7fbf5c016fe0b958c512555e460503235836d
huangbinxu <BinxuHuang@sf-express.com>
2019-04-12 17:14:00 +0800

    debug

--- index.js
+++ index.js
@@ -181,7 +181,7 @@ app.post('/weixin/upload', async (req, res) => {
     const port = await execPr(cmd);
     const options = {
         method: 'GET',
-        uri: `http://127.0.0.1:${port}/upload?projectpath=/root/jenkins/workspace/prod_fe__leshou-wx/output&version=${version}&desc=${desc}`,
+        uri: `http://127.0.0.1:${port}/upload?projectpath=%2Froot%2Fjenkins%2Fworkspace%2Fprod_fe__leshou-wx%2Foutput&version=${version}&desc=${desc}`,
     }
     let code = 0;
     let message = '上传成功，请到小程序管理后台进行体验版设置！';
