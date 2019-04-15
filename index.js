fe1a06ffcceacb379cdfe80e1f0f62c55203058d
huangbinxu <BinxuHuang@sf-express.com>
2019-04-15 20:02:21 +0800

    debug

--- index.js
+++ index.js
@@ -181,7 +181,7 @@ app.post('/weixin/upload', async (req, res) => {
     const port = await execPr(cmd);
     const options = {
         method: 'GET',
-        uri: `http://127.0.0.1:${port}/upload?version=${version}&desc=${desc}&projectpath=/projects/output`,
+        uri: `http://127.0.0.1:${port}/upload?version=${version}&desc=${desc}&projectpath=/projects`,
     }
     let code = 0;
     let message = '上传成功，请到小程序管理后台进行体验版设置！';
